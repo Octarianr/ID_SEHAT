bb16932e8818aca04c1d73302b7edbc8471631e4
Aziz Solihin <azizslhn45@gmail.com>
2021-10-21 01:11:11 +0800

    login system

--- ID_SEHAT/login-system/config.php
+++ ID_SEHAT/login-system/config.php
@@ -1,14 +1,12 @@
-<?php 
- 
+<?php
+
 $server = "localhost";
 $user = "root";
 $pass = "";
 $database = "login_idsehat";
- 
+
 $conn = mysqli_connect($server, $user, $pass, $database);
- 
+
 if (!$conn) {
     die("<script>alert('Gagal tersambung dengan database.')</script>");
 }
- 
-?>
